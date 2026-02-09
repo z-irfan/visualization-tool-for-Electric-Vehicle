498cf5210c5980ade7d17b89dd0968aedb4a3dcd
z-irfan <22x51a05d6@srecnandyal.edu.in>
2026-02-09 14:06:55 +0530

    Add files via upload

---
+++ EV_Project_Web/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app= Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/cta')
+def cta():
+    return render_template("cta.html")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
